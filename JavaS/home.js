2240ab70979a91b422bc57c63be3422f729d703d
Wiped-outt <rphlthrr@gmail.com>
2023-06-22 11:23:07 -0300

    a

--- JavaS/home.js
+++ JavaS/home.js
@@ -14,3 +14,32 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+
+var ProductImg = document.getElementById("ProductImg");
+
+
+var SmallImg = document.getElementsByClassName("SmallImg");
+
+  
+  SmallImg[0].onclick = function() {
+    
+    ProductImg.src = SmallImg[0].src;
+  }
+
+  
+  SmallImg[1].onclick = function() {
+    
+    ProductImg.src = SmallImg[1].src;
+  }
+
+  
+  SmallImg[2].onclick = function() {
+    
+    ProductImg.src = SmallImg[2].src;
+  }
+
+  
+  SmallImg[3].onclick = function() {
+
+    ProductImg.src = SmallImg[3].src;
+  }
